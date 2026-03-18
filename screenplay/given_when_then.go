8328b6cb456fc94d2886ecb13f313c4177808e52
Gwenol Grandperrin <6574028+grandper@users.noreply.github.com>
2026-03-18 13:32:47 +0100

    fix: change Given/When/Then/And to accept and return *Actor to enable method chaining

--- screenplay/given_when_then.go
+++ screenplay/given_when_then.go
@@ -1,21 +1,21 @@
 package screenplay
 
 // Given provides a convenient way to introduce "Given" statement in tests.
-func Given(actor Actor) Actor {
+func Given(actor *Actor) *Actor {
 	return actor
 }
 
 // When provides a convenient way to introduce "When" statement in tests.
-func When(actor Actor) Actor {
+func When(actor *Actor) *Actor {
 	return actor
 }
 
 // Then provides a convenient way to introduce "Then" statement in tests.
-func Then(actor Actor) Actor {
+func Then(actor *Actor) *Actor {
 	return actor
 }
 
 // And can be used to avoid repeating given, when, or then.
-func And(actor Actor) Actor {
+func And(actor *Actor) *Actor {
 	return actor
 }
